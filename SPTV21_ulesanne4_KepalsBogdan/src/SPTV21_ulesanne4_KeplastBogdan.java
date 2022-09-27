cbf54d361666b2371bc12b64a47596a86113d797
Bogdan Keplast <60263970+Bagai35@users.noreply.github.com>
2022-09-27 10:53:26 +0300

    Add files via upload

---
+++ SPTV21_ulesanne4_KepalsBogdan/src/SPTV21_ulesanne4_KeplastBogdan.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class SPTV21_ulesanne4_KeplastBogdan {
+    public static void main(String[] args) {
+        int[] nums;
+        int avg = 0, sum = 0;
+        nums = new int[100];
+        Random random = new Random();
+        int min = 1, max = 100;
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            nums[i] = random.nextInt(max - min + 1) - min;
+            sum +=nums[i];
+        }
+        avg = sum/100;
+        Arrays.sort(nums);
+        for (int s = 0; s < 10; s++) {
+            for (int v = 0; v < 10; v++) {
+                System.out.print(nums[s * 10 + v] + " ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("Среднее арифметическое: " + avg);
+    }
+}
